907bdaa3488868286ec9ceee3196f5fb7940bd5e
claytonoliver <claytonfra299@gmail.com>
2024-02-24 20:31:46 -0300

    Primeira etapada da controller programada

---
+++ ApiFilmes/Controllers/FilmeController.cs
@@ -0,0 +1,46 @@
+﻿using ApiFilmes.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+
+namespace ApiFilmes.Controllers;
+
+// primeiro passo importar anotações, depois alt + enter para importar aspnetcore.Mvc
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    //trabalhar requisição POST, GET...
+    [HttpPost()]
+    //indicar pelo frombody que a informação como parametro vem do corpo da requisição
+    public IActionResult AdicionarFilme([FromBody]Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        //recebe primeiro parametro no caso o metodo que retorna o filme por ID... Elemento, objetos/parametros, objeto criado no sistema
+        return CreatedAtAction(nameof(RecuperarFimePorId), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery]int skip = 0, [FromQuery] int take = 20)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    //para diferenciar precisa mudar o ID direto na URL /numero ID
+    [HttpGet("{id}")]
+    //? indica que pode ser ou não nulo
+
+    //IActionresult resultado de uma ação
+    public IActionResult RecuperarFimePorId(int id)
+    {
+        //filme => tem o filme.id == id do parametro
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok();
+    }
+}
---
+++ ApiFilmes/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFilmes.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    //data notations
+    [Required(ErrorMessage ="O Titulo do filme é obrigatório")]
+    [MaxLength(80, ErrorMessage = "Tamanho máximo de caracteres inválidos")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Titulo do genero é obrigatório")]
+    [MaxLength(50,ErrorMessage ="Tamanho máximo de caracteres inválidos")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage ="O filme deve ter entre 70 e 600 segundos")]
+    public int Duracao { get; set;}
+    
+}
